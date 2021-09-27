13c72519e60982c1b154307d9f15de194631a844
Aleyna Ceylan <aceylan1@binghamton.edu>
2021-09-26 20:08:50 -0400

    Changes Committed
    View Changes Added 
    Navigation Controller Added 
    Input from Text Field is being added to the string array.

--- ShoppingList/ShoppingList/ViewController.swift
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ShoppingList
 //
-//  Created by Aleyna  Ceylan on 9/25/21.
+//  Created by Aleyna Ceylan on 9/25/21.
 
 
 import UIKit
@@ -10,7 +10,8 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var shoppingList = [String]()
-
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var CountField: UITextField!
     @IBOutlet weak var TableViewList: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +19,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         TableViewList.delegate = self
         TableViewList.dataSource = self
         
-        shoppingList.append("Apple")
-        shoppingList.append("Banana")
-        shoppingList.append("Pie")
-        
+    }
+    @IBAction func SetAddButton(_ sender: Any) {
+        shoppingList.append(userInput.text!)
+    }
+
+    @IBAction func setSendButton(_ sender: UIButton, forEvent event: UIEvent) {
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            shoppingList.remove(at:indexPath.row)
+            TableViewList.deleteRows(at: [indexPath], with: UITableView.RowAnimation.top)
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,7 +42,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingList.count
     }
-
     
+    func textFieldShouldReturn(_ userInput: UITextField) -> Bool {
+        userInput.resignFirstResponder() // dismiss keyboard
+        return true
+    }
 }
 
---
+++ ShoppingList/ShoppingList/sendViewController.swift
@@ -0,0 +1,39 @@
+//
+//  sendViewController.swift
+//  ShoppingList
+//
+//  Created by Aleyna  Ceylan on 9/26/21.
+//
+
+import UIKit
+
+class sendViewController: UIViewController {
+
+    @IBOutlet weak var MailHeader: UILabel!
+    @IBOutlet weak var MessageHeader: UILabel!
+    
+    @IBOutlet weak var setMesField: UITextField!
+    @IBOutlet weak var setMailField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func setMailButton(_ sender: Any) {
+    }
+    @IBAction func setMessageButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
